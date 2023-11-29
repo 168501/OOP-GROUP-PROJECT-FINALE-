2edcc95e05cab81a93443272314ed29110128baa
abdulrauf <abdi.abdulrauf@strathmore.edu>
2023-11-29 20:51:30 +0300

    abdulrauf adding main.java

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        Hotel hotel = Hotel.createHotel();
+
+        System.out.println("Hotel created successfully!");
+
+
+        hotel.displayRoomInfo();
+
+
+        System.out.println("Enter your name:");
+        String guestName = scanner.nextLine();
+
+        System.out.println("Enter your email:");
+        String guestEmail = scanner.nextLine();
+
+        System.out.println("Enter the room number to book (between 0 and 99):");
+        int roomToBook = scanner.nextInt();
+
+        if (roomToBook >= 0 && roomToBook < 100) {
+            System.out.println("available.");
+
+
+
+
+
+
+        }else {
+            System.out.println("Invalid room number. Room number should be between 0 and 99.");
+        }
+
+
+        hotel.displayRoomInfo();
+    }
+}
